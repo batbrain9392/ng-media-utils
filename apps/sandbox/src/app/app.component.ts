aa56ee99f46dcadc2810c2b4c66fd251426b5143
batbrain9392 <batbrain9392@gmail.com>
2022-02-18 23:44:58 +0000

    reset

--- apps/sandbox/src/app/app.component.ts
+++ apps/sandbox/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ng-media-utils-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'sandbox';
-}
+export class AppComponent {}
