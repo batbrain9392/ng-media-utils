7706808f60262ca0d4e68a6c8fe493b23be950cb
batbrain9392 <batbrain9392@gmail.com>
2022-02-19 01:04:51 +0000

    first draft of background blur

--- apps/sandbox/src/app/app.component.ts
+++ apps/sandbox/src/app/app.component.ts
@@ -1,8 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Results, SelfieSegmentation } from '@mediapipe/selfie_segmentation';
+import { MEDIA_DEVICES } from '@ng-web-apis/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  stream: MediaStream | undefined;
+  selfieSegmentation = new SelfieSegmentation({
+    locateFile: (file) => {
+      return `/assets/background-blur/${file}`;
+    },
+  });
+  globalController: TransformStreamDefaultController<VideoFrame> | undefined;
+  timestamp: number | undefined;
+  canvasElement2 = new OffscreenCanvas(1280, 720);
+  canvasCtx2 = this.canvasElement2.getContext('2d');
+
+  constructor(
+    @Inject(MEDIA_DEVICES) private readonly mediaDevices: MediaDevices
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    this.selfieSegmentation.setOptions({
+      modelSelection: 1,
+      selfieMode: false,
+    });
+    this.selfieSegmentation.onResults((v) => this.onResults(v));
+    await this.selfieSegmentation.initialize();
+    const feed = await this.mediaDevices.getUserMedia({
+      video: {
+        aspectRatio: {
+          ideal: 16 / 9,
+        },
+        height: {
+          ideal: 720,
+        },
+      },
+    });
+    const [track] = feed.getVideoTracks();
+    const trackProcessor = new MediaStreamTrackProcessor({ track });
+    const trackGenerator = new MediaStreamTrackGenerator({ kind: 'video' });
+    const transformer = new TransformStream<VideoFrame, VideoFrame>({
+      transform: async (videoFrame, controller) => {
+        // @ts-expect-error Property 'width' does not exist on type 'VideoFrame'
+        videoFrame.width = videoFrame.displayWidth;
+        // @ts-expect-error Property 'height' does not exist on type 'VideoFrame'
+        videoFrame.height = videoFrame.displayHeight;
+        this.timestamp = videoFrame.timestamp ?? undefined;
+        this.globalController = controller;
+        await this.selfieSegmentation.send({
+          image: videoFrame as unknown as HTMLImageElement,
+        });
+        videoFrame.close();
+      },
+    });
+    trackProcessor.readable
+      .pipeThrough(transformer)
+      .pipeTo(trackGenerator.writable);
+    this.stream = new MediaStream([trackGenerator]);
+  }
+
+  ngOnDestroy(): void {
+    this.selfieSegmentation.close();
+  }
+
+  onResults(results: Results) {
+    if (!this.canvasCtx2) {
+      return;
+    }
+    this.canvasElement2.width = results.image.width;
+    this.canvasElement2.height = results.image.height;
+    this.canvasCtx2.save();
+    this.canvasCtx2.clearRect(
+      0,
+      0,
+      this.canvasElement2.width,
+      this.canvasElement2.height
+    );
+    this.canvasCtx2.drawImage(
+      results.segmentationMask,
+      0,
+      0,
+      this.canvasElement2.width,
+      this.canvasElement2.height
+    );
+
+    // Only overwrite existing pixels.
+    this.canvasCtx2.globalCompositeOperation = 'source-in';
+    this.canvasCtx2.drawImage(
+      results.image,
+      0,
+      0,
+      this.canvasElement2.width,
+      this.canvasElement2.height
+    );
+
+    // Only overwrite missing pixels.
+    this.canvasCtx2.globalCompositeOperation = 'destination-atop';
+    this.canvasCtx2.fillStyle = '#00FF00';
+    this.canvasCtx2.fillRect(
+      0,
+      0,
+      this.canvasElement2.width,
+      this.canvasElement2.height
+    );
+
+    this.canvasCtx2.restore();
+    this.globalController?.enqueue(
+      new VideoFrame(this.canvasElement2.transferToImageBitmap(), {
+        timestamp: this.timestamp,
+        alpha: 'discard',
+      })
+    );
+  }
+}
