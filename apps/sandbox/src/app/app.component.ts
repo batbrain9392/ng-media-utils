5da2bb3e3ca4fe1904c5a056d1299fce3351ab56
batbrain9392 <batbrain9392@gmail.com>
2022-02-20 23:44:43 +0000

    works with cam start & stop

--- apps/sandbox/src/app/app.component.ts
+++ apps/sandbox/src/app/app.component.ts
@@ -8,16 +8,20 @@ import { MEDIA_DEVICES } from '@ng-web-apis/common';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  stream: MediaStream | undefined;
-  selfieSegmentation = new SelfieSegmentation({
+  private readonly canvasElement2 = new OffscreenCanvas(0, 0);
+  private readonly canvasCtx2 = this.canvasElement2.getContext('2d');
+  private readonly selfieSegmentation = new SelfieSegmentation({
     locateFile: (file) => {
       return `/assets/background-blur/${file}`;
     },
   });
-  globalController: TransformStreamDefaultController<VideoFrame> | undefined;
-  timestamp: number | undefined;
-  canvasElement2 = new OffscreenCanvas(1280, 720);
-  canvasCtx2 = this.canvasElement2.getContext('2d');
+  private globalController:
+    | TransformStreamDefaultController<VideoFrame>
+    | undefined;
+  private timestamp: number | undefined;
+
+  originalStream: MediaStream | undefined;
+  processedStream: MediaStream | undefined;
 
   constructor(
     @Inject(MEDIA_DEVICES) private readonly mediaDevices: MediaDevices
@@ -30,41 +34,11 @@ export class AppComponent implements OnInit, OnDestroy {
     });
     this.selfieSegmentation.onResults((v) => this.onResults(v));
     await this.selfieSegmentation.initialize();
-    const feed = await this.mediaDevices.getUserMedia({
-      video: {
-        aspectRatio: {
-          ideal: 16 / 9,
-        },
-        height: {
-          ideal: 720,
-        },
-      },
-    });
-    const [track] = feed.getVideoTracks();
-    const trackProcessor = new MediaStreamTrackProcessor({ track });
-    const trackGenerator = new MediaStreamTrackGenerator({ kind: 'video' });
-    const transformer = new TransformStream<VideoFrame, VideoFrame>({
-      transform: async (videoFrame, controller) => {
-        // @ts-expect-error Property 'width' does not exist on type 'VideoFrame'
-        videoFrame.width = videoFrame.displayWidth;
-        // @ts-expect-error Property 'height' does not exist on type 'VideoFrame'
-        videoFrame.height = videoFrame.displayHeight;
-        this.timestamp = videoFrame.timestamp ?? undefined;
-        this.globalController = controller;
-        await this.selfieSegmentation.send({
-          image: videoFrame as unknown as HTMLImageElement,
-        });
-        videoFrame.close();
-      },
-    });
-    trackProcessor.readable
-      .pipeThrough(transformer)
-      .pipeTo(trackGenerator.writable);
-    this.stream = new MediaStream([trackGenerator]);
   }
 
-  ngOnDestroy(): void {
-    this.selfieSegmentation.close();
+  async ngOnDestroy(): Promise<void> {
+    this.stopCam();
+    await this.selfieSegmentation.close();
   }
 
   onResults(results: Results) {
@@ -115,5 +89,53 @@ export class AppComponent implements OnInit, OnDestroy {
         alpha: 'discard',
       })
     );
+  }
+
+  async startCam() {
+    this.originalStream = await this.mediaDevices.getUserMedia({
+      video: {
+        aspectRatio: {
+          ideal: 16 / 9,
+        },
+        height: {
+          ideal: 720,
+        },
+        frameRate: {
+          ideal: 15,
+        },
+      },
+      audio: true,
+    });
+    const [videoTrack] = this.originalStream.getVideoTracks();
+    const [audioTrack] = this.originalStream.getAudioTracks();
+    const trackProcessor = new MediaStreamTrackProcessor({ track: videoTrack });
+    const trackGenerator = new MediaStreamTrackGenerator({ kind: 'video' });
+    const transformer = new TransformStream<VideoFrame, VideoFrame>({
+      transform: async (videoFrame, controller) => {
+        // @ts-expect-error Property 'width' does not exist on type 'VideoFrame'
+        videoFrame.width = videoFrame.displayWidth;
+        // @ts-expect-error Property 'height' does not exist on type 'VideoFrame'
+        videoFrame.height = videoFrame.displayHeight;
+        this.timestamp = videoFrame.timestamp ?? undefined;
+        this.globalController = controller;
+        await this.selfieSegmentation.send({
+          image: videoFrame as unknown as HTMLImageElement,
+        });
+        videoFrame.close();
+      },
+      flush: (controller) => {
+        controller.terminate();
+        this.selfieSegmentation.reset();
+      },
+    });
+    trackProcessor.readable
+      .pipeThrough(transformer)
+      .pipeTo(trackGenerator.writable);
+    this.processedStream = new MediaStream([trackGenerator, audioTrack]);
+  }
+
+  stopCam() {
+    this.originalStream?.getTracks().forEach((track) => track.stop());
+    this.processedStream?.getTracks().forEach((track) => track.stop());
   }
 }
